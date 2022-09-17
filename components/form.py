7126efd07e31aafe78c7abec37ecb2d165dc8648
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-09-17 23:23:19 +0200

    Removed nav_page func from components/form

--- components/form.py
+++ components/form.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from utils import app
 from utils import sb
-from pages import _1_Select
 from streamlit.components.v1 import html
 
 
@@ -9,30 +8,30 @@ from streamlit.components.v1 import html
 
 # Func to navigate to another page
 
-def _nav_page(page_name, timeout_secs=3):
-    nav_script = """
-        <script type="text/javascript">
-            function attempt_nav_page(page_name, start_time, timeout_secs) {
-                var links = window.parent.document.getElementsByTagName("a");
-                for (var i = 0; i < links.length; i++) {
-                    if (links[i].href.toLowerCase().endsWith("../pages/" + page_name.toLowerCase())) {
-                        links[i].click();
-                        return;
-                    }
-                }
-                var elasped = new Date() - start_time;
-                if (elasped < timeout_secs * 1000) {
-                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
-                } else {
-                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
-                }
-            }
-            window.addEventListener("load", function() {
-                attempt_nav_page("%s", new Date(), %d);
-            });
-        </script>
-    """ % (page_name, timeout_secs)
-    html(nav_script)
+# def _nav_page(page_name, timeout_secs=3):
+#     nav_script = """
+#         <script type="text/javascript">
+#             function attempt_nav_page(page_name, start_time, timeout_secs) {
+#                 var links = window.parent.document.getElementsByTagName("a");
+#                 for (var i = 0; i < links.length; i++) {
+#                     if (links[i].href.toLowerCase().endsWith("../pages/" + page_name.toLowerCase())) {
+#                         links[i].click();
+#                         return;
+#                     }
+#                 }
+#                 var elasped = new Date() - start_time;
+#                 if (elasped < timeout_secs * 1000) {
+#                     setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+#                 } else {
+#                     alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+#                 }
+#             }
+#             window.addEventListener("load", function() {
+#                 attempt_nav_page("%s", new Date(), %d);
+#             });
+#         </script>
+#     """ % (page_name, timeout_secs)
+#     html(nav_script)
 
 
 # -----------------------------------------------------------------------
